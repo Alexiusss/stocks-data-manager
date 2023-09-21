fa7651099aa604a7f64299056a8d981156db8bb8
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-09-21 21:33:15 +0300

    Init

---
+++ src/main/java/com/example/stocksdatamanager/StocksDataManagerApplication.java
@@ -0,0 +1,12 @@
+package com.example.stocksdatamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksDataManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksDataManagerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/stocksdatamanager/StocksDataManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.stocksdatamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksDataManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
