1c1fa0a04caad3a76fb88956dfa40b69805c345d
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-09-26 22:07:11 +0300

    Added methods for converting JSON to Objects in JsonUtil

--- src/main/java/com/example/stocksdatamanager/util/JsonUtil.java
+++ src/main/java/com/example/stocksdatamanager/util/JsonUtil.java
@@ -1,7 +1,11 @@
 package com.example.stocksdatamanager.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
 import lombok.experimental.UtilityClass;
+
+import java.io.IOException;
+import java.util.List;
 
 @UtilityClass
 public class JsonUtil {
@@ -9,5 +13,22 @@ public class JsonUtil {
 
     public static void setMapper(ObjectMapper mapper) {
         JsonUtil.mapper = mapper;
+    }
+
+    public static <T> T readValue(String json, Class<T> clazz) {
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Invalid read array from JSON:\n'" + json + "'", e);
+        }
+    }
+
+    public static <T> List<T> readValues(String json, Class<T> clazz) {
+        ObjectReader reader = mapper.readerFor(clazz);
+        try {
+            return reader.<T>readValues(json).readAll();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Invalid read array from JSON:\n'" + json + "'", e);
+        }
     }
 }
