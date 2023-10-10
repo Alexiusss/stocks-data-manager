8107e7a7a2f60e0a9924f8bdc50787901dbf5ad7
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-10-10 21:30:17 +0300

    Added SchedulerConfig

---
+++ src/main/java/com/example/stocksdatamanager/config/SchedulerConfig.java
@@ -0,0 +1,13 @@
+package com.example.stocksdatamanager.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
+@Profile("!test")
+public class SchedulerConfig {
+}
